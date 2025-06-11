40fc9d2d3628d1ebec375f3be56700ad219d5e45
Juan Cabanela <juan@cabanela.com>
2025-06-11 14:21:01 -0500

    Updated existing app.py to move its utility functions and configuration information to separate files.

---
+++ config.py
@@ -0,0 +1,6 @@
+# Define the base URL for course detail pages
+# The placeholders in the URL will be replaced with actual course data
+COURSE_DETAIL_URL = 'https://eservices.minnstate.edu/registration/search/detail.html?campusid=072&courseid={course_id}&yrtr={year_term}&rcid=0072&localrcid=0072&partnered=false&parent=search'
+
+# Define the directory where cached CSV files will be stored
+CACHE_DIR = 'viewed-csvs'

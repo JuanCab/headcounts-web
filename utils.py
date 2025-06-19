15a7a9e97f5eeaacb3db1d1cd68a35d43aeb1d5e
Juan Cabanela <juan.cabanela@go.mnstate.edu>
2025-06-19 08:23:05 -0500

    Added ability to filter by college code.  Also in progress in migrating common_response and its dependencies to work with Polars instead of astropy.

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import datetime
-import os
 import re
+from pathlib import Path
 
 import numpy as np
 from flask import render_template
@@ -20,9 +20,6 @@ def filter_data(tbl, subject, spec1=None, spec2=None):
     """
     This takes the original Polars Lazy DataFrame and filters it down to
     only the rows that match the subject and specifications provided.
-    The subject can be a course subject (e.g., 'CSCI'), a LASC area
-    ('lasc'), a WI course ('wi'), or '18online' for online courses. If
-    the subject is 'all', it returns the entire table.
 
     Parameters
     ----------
@@ -31,10 +28,11 @@ def filter_data(tbl, subject, spec1=None, spec2=None):
 
     subject : str
         The subject to filter by, which can be a course subject (e.g.,
-        'CSCI'), a LASC area ('lasc'), a WI course ('wi'), or '18online'
-        for online courses. Setting it to 'all' will return the
-        entire table without filtering (pending other specified filters
-        below).
+        'CSCI'), a 4-letter MSUM college code (valid codes are 'CBAC',
+        'COAH', 'CSHE', 'CEHS', or 'NONE'), a LASC area ('lasc'), a WI
+        course ('wi'), or '18online' for online courses. Setting it to
+        'all' will return the entire table without filtering (pending
+        other specified filters below).
 
     spec1 : str, optional
         The first specification to filter by, which can be a course
@@ -53,19 +51,26 @@ def filter_data(tbl, subject, spec1=None, spec2=None):
         operation lazily, meaning it does not immediately execute the
         filtering operation until the data is actually needed.
 
-
-    The default behavior is to filter by the subject provided. If
-    the subject is 'lasc', it filters for LASC courses; if 'wi', it
-    filters for WI courses; if '18online', it filters for online
-    courses; if 'all', it returns the entire table; otherwise, it
-    filters by the specified course subject.
+    The default behavior is to filter by the subject provided.
+    - If the subject is a college code, it filters by college;
+    - If the subject is 'lasc', it filters for LASC courses;
+    - if 'wi', it filters for WI courses;
+    - if '18online', it filters for online courses;
+    - if 'all', it returns the entire table;
+    otherwise, it filters by the specified course subject.
     """
+
+    # MSUM Colleges
+    MSUM_COLLEGES = [ 'cbac', 'coah', 'cshe', 'cehs', 'none' ]
 
     # Make sure subject is lowercase
     subject = subject.lower()
 
     # Determine the subject category and filter accordingly
-    if subject == 'lasc':
+    if subject in MSUM_COLLEGES:
+        # If the subject is a college code, filter by that college
+        filtered_table = tbl.filter(pl.col('College')== subject.upper())
+    elif subject == 'lasc':
         return tbl.filter(
             (pl.col("LASC/WI").is_not_null()) & (pl.col("LASC/WI") != "WI")
             )
@@ -125,6 +130,12 @@ def filter_data(tbl, subject, spec1=None, spec2=None):
                         pl.col('#') == spec
                     )
 
+    # Always sort the output by Fiscal yrtr, Subj, #, and section
+    filtered_table = filtered_table.sort(
+        by=['Fiscal yrtr', 'Subj', '#', 'Sec'],
+        descending=[False, False, False, False]
+    )
+
     # Return the filtered LazyFrame
     return filtered_table
 
@@ -143,32 +154,29 @@ def filled_credits(credit_column, variable_credits=1):
         The value to use for variable credits, by default 1.
 
     Returns
-    ------- 
-    list of int
-        This function returns a list of integers representing the
-        credits for each course. Variable credits (represented as 'Vari.')
-        in the original column are replaced with the specified value
-        (`variable_credits`), and all credits are rounded to the nearest
-        valid integer.
+    -------
+    Polars Series
+        This function returns a Polars Series of integers representing
+        the credits for each course. Variable credits (represented as
+        'Vari.') in the original column are replaced with the specified
+        value (`variable_credits`), and all credits are rounded to the
+        nearest valid integer.
     """
-    # Identify rows in Polars series where the credit value is 'Vari.'
-    vari_rows = credit_column == 'Vari.'
+    # Create a float version of the column, replacing 'Vari.' with the
+    # specified variable credits value. Convert the float version into
+    # a series of integers by ROUNDING to the nearest integer.
+    result = (
+        credit_column.str.replace("Vari\\.", "1")
+        .cast(pl.Float64, strict=False)
+    ).round(0).cast(pl.Int64).alias('IntCrd')
 
-    # Create a copy of the credit column, masking the variable rows
-    # and filling them with the specified variable credits value
-    crds = credit_column.copy()
-    crds.mask[vari_rows] = True
-    crds.fill_value = variable_credits
-
-    # Convert the filled column to float, round it, and then convert
-    # to int
-    crds = np.round(crds.filled().astype(float)).astype(int)
-    return crds
+    # Return the column as rounded integers, converted to a numpy array
+    return result
 
 
 def calc_sch(table, vari_credits=1):
     """
-    Calculate total student credit hours generated by courses in this 
+    Calculate total student credit hours generated by courses in this
     table.
 
     Parameters
@@ -187,7 +195,7 @@ def calc_sch(table, vari_credits=1):
     """
 
     # Fill the 'Crds' column with variable credits where applicable
-    crds = filled_credits(table['Crds'], variable_credits=vari_credits)
+    crds = filled_credits(table['Credits'], variable_credits=vari_credits)
 
     # Calculate the total student credit hours by multiplying
     # the number of enrolled students by their respective credits
@@ -227,7 +235,7 @@ def calc_seats(table):
     positive = empty > 0
     empty *= positive
 
-    # Sum the empty, filled, and available seats for courses that are 
+    # Sum the empty, filled, and available seats for courses that are
     # not canceled
     empty = empty[not_canceled].sum()
     available = table['Size:'][not_canceled].sum()
@@ -242,9 +250,9 @@ def calc_tuition(table, variable_credits=1):
 
     Parameters
     ----------
-    table : astropy Table
-        The table containing course data, including 'Tuition unit',
-        'Crds', 'Tuition -resident', and 'Enrolled' columns.
+    table : polars DataFrame
+        The table containing course data, including 'Credits', 'Enrolled',
+        'Tuition unit', and 'Tuition Resident' columns.
     variable_credits : int, optional
         The value to use for variable credits, by default 1.
 
@@ -255,49 +263,82 @@ def calc_tuition(table, variable_credits=1):
         students multiplied by their respective tuition amounts, adjusted
         for credit hours if applicable.
     """
-    by_credit = table['Tuition unit'] == 'credit'
-    credits = filled_credits(table['Crds'],
+
+    # NOTE: We used to mask "n/a" tuition values, but now they are set
+    # to zero, which results in the same effect.
+
+    # Create a Polars series of credits, filling in variable credits
+    # filled with the specified value with "Vari." credit is set
+    credits = filled_credits(table["Credits"],
                              variable_credits=variable_credits)
-    tuition_nas = table['Tuition -resident'] == 'n/a'
-    table['Tuition -resident'].mask |= tuition_nas
-    # parse tuition as a string into a number
-    money = [float(m.replace('$', '').replace(',', '')) for m in table['Tuition -resident'].filled(fill_value='0')]
-    money *= table['Enrolled']
-    money[by_credit] *= credits[by_credit]
-    return money.sum()
+    # Add this series to the table as a new column
+    table = table.with_columns(credits.alias('Integer Credits'))
+
+    # The unit for tuition can either be 'course' or 'credit'. So to
+    # compute the total tuition, we need to add up all the tuition
+    # values for each course, multiplied by the number of enrolled
+    # students, and then multiply by the number of credits for each
+    # course if the unit is 'credit'. If the unit is 'course', we just
+    # multiply by the number of enrolled students. We will assume
+    # residential tuition for now.
+    table = table.with_columns(
+        # Tuition by course
+        pl.when(pl.col("Tuition unit") == "course")
+        .then(pl.col("Tuition Resident") * pl.col("Enrolled"))
+        # Tuition by credit
+        .otherwise(pl.col("Tuition Resident") * pl.col("Integer Credits")
+                   * pl.col("Enrolled"))
+        .alias("Tuition Charged")
+    )
+
+    # Return calculated tuition revenue by summing the 'Tuition Charged'
+    return table['Tuition Charged'].sum()
 
 
-def gen_cache_file(path, table):
+def generate_csv_file(table, path, dir=CACHE_DIR):
     """
-    Generate a name for a cache of a view of the data based on
-    the URL path to the view
+    Generate a CSV file containing all the data in this dataframe
+    and save it to the cache directory. The filename is generated
+    based on the URL path and the average timestamp of the courses in
+    the table. The average timestamp is used to ensure that the filename
+    is unique for each view, even if the same path is accessed multiple
+    times.
 
     Parameters
     ----------
+    table : polars Dataframe
+        The polars dataframe containing course data to be cached.
     path : str
         The URL path that got the user here, used to generate a unique
         filename for the cached data.
-    table : astropy Table
-        The table containing course data to be cached.
+    dir : str, optional
+        The directory where the CSV file will be saved. Defaults to
+        the CACHE_DIR defined in the config.
 
     Returns
     -------
     str
-        The name of the cached file, which is a CSV file containing the
-        course data for the specified view.
+        The name of the CSV datafile containing the course data for the
+        specified view.
     """
 
-    # Compute the average time for all courses in the table.
-    avg_time = int(table['timestamp'].mean())
+    # Compute the average time for all courses in the dataframe based
+    # on the "Last Updated" column.
+    avg_time = int(table.select(pl.col('Last Updated')).mean().item().timestamp())
 
     # path always starts with a leading /, remove it
     rel_path = path[1:]
     parts = rel_path.split('/')
-    parts.extend([str(avg_time)])
+    parts.append(str(avg_time))
     name = '-'.join(parts) + '.csv'
-    file_path = os.path.join(CACHE_DIR, name)
-    if not os.path.isfile(file_path):
-        table.write(file_path)
+    file_path = Path(CACHE_DIR) / name
+
+    # Check if file already exists, if not, write the dataframe to a
+    # CSV file
+    if not file_path.is_file():
+        table.write_csv(file_path)
+
+    # Return the name of the CSV file
     return name
 
 
@@ -308,32 +349,60 @@ def common_response(render_me, path):
 
     Parameters
     ----------
-    render_me : astropy Table
+    render_me : polars DataFrame
         The table to be rendered in the view.
 
     path : str
         The URL path that got the user here.
 
+
     Returns
     -------
     str
-        The rendered HTML template for the course information page,
-        including the table, timestamp, most recent term, year terms,
-        cache file name, student credit hours, available seats, and
-        tuition revenue.
+        The rendered HTML template for the course information page.
+
+    This functions processes the provided Polars DataFrame of course
+    data, calculates various statistics such as student credit hours,
+    available seats, and tuition revenue, and then renders an HTML
+    template with this information.
+
+    It also generates a cached CSV file of the data for download.
     """
-    terms = sorted(set(render_me['year_term']))
-    year_terms = ', '.join([parse_year_term(str(t)) for t in terms])
-    file_name = gen_cache_file(path, render_me)
-    most_recent = int(render_me['timestamp'].max())
-    most_recent = datetime.datetime.fromtimestamp(most_recent)
-    oldest = render_me['timestamp'].min()
-    return render_template('course_info.html', table=render_me,
+
+    # Determine all the unique 'Term' in this polars Dataframe, sorted
+    # by Fiscal year/term,
+    terms = (
+        render_me.sort('Fiscal yrtr')
+        .select(pl.col('Term'))
+        .unique()
+        .to_series()
+        .to_list()
+    )
+
+    # Generate the CSV file corresponding to this data using full
+    # dataset
+    csv_filename = generate_csv_file(render_me, path)
+
+    # Get most recent and oldest timestamps from the DataFrame
+    most_recent = render_me.select(pl.col('Last Updated')).max().item()
+    oldest = render_me.select(pl.col('Last Updated')).min().item()
+
+    # If the table is larger than 100 rows, only render the first 100
+    # rows to avoid performance issues in the browser.
+    max_rows = 100
+    if render_me.height > max_rows:
+        render_me = render_me.head(max_rows)
+
+    # Render the page using the 'course_info.html' template,
+    return render_template('course_info.html',
+                           table=render_me,
                            timestamp=int(render_me['timestamp'].mean()),
+                           oldest=oldest,
+                           max_rows=max_rows,
                            most_recent=most_recent,
-                           year_term=year_terms,
+                           year_term=terms,
                            sch=calc_sch(render_me),
-                           filename=file_name,
+                           filename=csv_filename,
                            seats=calc_seats(render_me),
                            revenue=calc_tuition(render_me),
                            base_detail_url=COURSE_DETAIL_URL)
